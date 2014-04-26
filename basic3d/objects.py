c059e6c1c0570a9d69acf32b7e3dbacd443776ac
Bertie Wheen <wheen.b@gmail.com>
2014-04-26 20:45:26 +0100

    Added rotation methods

--- basic3d/objects.py
+++ basic3d/objects.py
@@ -1,8 +1,32 @@
+from math import sin, cos
+
+#############################
+# All angles are in radians #
+#############################
+
 class Vec3(object):
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
+
+    def rotateX(self, theta):
+        x = self.x
+        y = self.y*cos(theta) - self.z*sin(theta)
+        z = self.y*sin(theta) + self.z*cos(theta)
+        self.x, self.y, self.z = x, y, z
+
+    def rotateY(self, theta):
+        x = self.x*cos(theta) + self.z*sin(theta)
+        y = self.y
+        z = -self.x*sin(theta) + self.z*cos(theta)
+        self.x, self.y, self.z = x, y, z
+
+    def rotateZ(self, theta):
+        x = self.x*cos(theta) - self.y*sin(theta)
+        y = self.x*sin(theta) + self.y*cos(theta)
+        z = self.z
+        self.x, self.y, self.z = x, y, z
 
 class Face(object):
     def __init__(self, vertices):
@@ -20,3 +44,10 @@ class Obj3D(object):
 
     def handle_event(self, event):
         pass
+
+    def rotate(self, xTheta, yTheta, zTheta):
+        for face in self.faces:
+            for vertex in face.vertices:
+                vertex.rotateZ(zTheta)
+                vertex.rotateY(yTheta)
+                vertex.rotateX(xTheta)
