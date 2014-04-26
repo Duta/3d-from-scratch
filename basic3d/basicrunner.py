9b9897470aceff97bf6da542899fad459e186e9a
Bertie Wheen <wheen.b@gmail.com>
2014-04-26 22:40:31 +0100

    More comments

--- basic3d/basicrunner.py
+++ basic3d/basicrunner.py
@@ -12,24 +12,35 @@ def main_loop(w, h, title, obj3Ds, fps=60):
     # Set the window's title
     pygame.display.set_caption(title)
 
-    # Render function
     def render(obj3Ds):
+        # Clear the screen
         screen.fill(colors.white)
 
+        # The co-ordinates of the center of the screen
         cx, cy = w/2, h/2
+        # Render each object in the scene
         for obj3D in obj3Ds:
+            # The object's location
             ox, oy, oz = obj3D.x, obj3D.y, obj3D.z
+            # Render each face of the object
             for face in obj3D.faces:
+                # The face's vertices
                 vertices = face.vertices
-                xys = []
+                # The scale factor
                 sf = min(cx, cy)
+                # Convert the vertices' locations
+                # to 2d screen co-ordinates
+                xys = []
                 for vertex in vertices:
+                    # The vertex's location
                     vx = vertex.x + ox
                     vy = vertex.y + oy
                     vz = vertex.z + oz
+                    # Project to 2d
                     x = cx + sf*vx/vz
                     y = cy + sf*vy/vz
                     xys.append([x, y])
+                # Draw the face to the screen
                 pygame.draw.polygon(screen, colors.black, xys, 1)
 
     # Main loop
