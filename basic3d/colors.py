b06afeb7c366d436fbc2ae7d23d566dddea0dbca
Bertie Wheen <wheen.b@gmail.com>
2014-04-26 22:54:25 +0100

    More colours

--- basic3d/colors.py
+++ basic3d/colors.py
@@ -1,5 +1,32 @@
-white = (255, 255, 255)
-black = (  0,   0,   0)
-red   = (255,   0,   0)
-green = (  0, 255,   0)
-blue  = (  0,   0, 255)
+black       =   0,   0,   0
+white       = 255, 255, 255
+
+blue        =   0,   0, 255
+lime        =   0, 255,   0
+red         = 255,   0,   0
+
+cyan        =   0, 255, 255
+yellow      = 255, 255,   0
+magenta     = 255,   0, 255
+
+gray        = 128, 128, 128
+silver      = 192, 192, 192
+
+maroon      = 128,   0,   0
+green       =   0, 128,   0
+navy        =   0,   0, 128
+
+teal        =   0, 128, 128
+olive       = 128, 128,   0
+purple      = 128,   0, 128
+
+brown       = 165,  42,  42
+
+dark_red    = 139,   0,   0
+dark_green  =   0, 100,   0
+dark_blue   =   0,   0, 139
+
+orange_red  = 255,  69,   0
+dark_orange = 255, 140,   0
+orange      = 255, 165,   0
+gold        = 255, 215,   0
